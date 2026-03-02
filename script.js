1603b2a5e6747ec850ce81aa4e9e7cba1ce3a06c
mapss-blip <mp.sanabrias1@uniandes.edu.co>
2026-03-02 16:47:05 +0000

    java1

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("holaa");
+const varTitulo = document.getElementById("titulo");
+//console.log(varTitulo);
+
+varTitulo.addEventListener("click", () => {
+varTitulo.innerText = "¡Medio litro de helado por tan solo 2000 pesitos!";
+varTitulo.style.backgroundColor= "pink"
+})
