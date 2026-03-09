c4d1d5ebba6c5eb5c30f721072987f8cba2ddfd8
mapss-blip <mp.sanabrias1@uniandes.edu.co>
2026-03-09 15:46:17 +0000

    Agregar textossss

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 //console.log("holaa");
 const varTitulo = document.getElementById("titulo");
-//console.log(varTitulo);
+const varTextooo = document.getElementById("textooo");
+
+const listaTextoss =["Esta es la hisoria de Manolito", "Manolito está maldito", "Está condenado a siempre sonrojarse cuando se le acercan ", "Cuando se sonroja le crece la cabeza", "Y la cabeza le da vueltas porque esto no lo puede controlar", "entonces ahora esá condenado al sonrojo perpetuo", "Y a verse como el meme del hamster de ojos grandes y llorosos"];
+
+let estadoInicial = true;
+let indice = 0
 
 varTitulo.addEventListener("click", () => {
-varTitulo.innerText = "¡Medio litro de helado por tan solo 2000 pesitos!";
-varTitulo.style.backgroundColor= "pink"
+    if(estadoInicial === true){
+        varTitulo.innerText = "¡Medio litro de helado por tan solo 2000 pesitos!";
+        varTitulo.style.backgroundColor= "pink";
+        varTitulo.style.color="royalblue";
+    }
+    else {
+        varTitulo.innerText = "Manolito";
+        varTitulo.style.backgroundColor= "transparent";
+        varTitulo.style.color="rgb(85, 17, 119)";
+    }
+    estadoInicial = !estadoInicial;
+})
+
+listaTextoss[listaTextoss[0]];
+varTextooo.addEventListener("click", () =>{
+    if (indice < listaTextoss.length){
+         varTextooo.innerText= listaTextoss[indice];
+        indice = indice +1;
+    }
+    else {
+        varTextooo.innerText = "el fin <3";
+    }
+
 })
